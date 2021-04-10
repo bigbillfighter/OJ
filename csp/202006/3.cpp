82f581aaef1dcd5533efd9c6cdcdeda9b01197fe
lucas <jxj1950970529@gmail.com>
2021-04-10 22:28:46 +0800

    202004112228

---
+++ csp/202006/3.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+csp
+
+csp is
+a real realrealrealrealreal 
+     competition.
+
+   
+Come   and   join   us   
+
+
+*/
+/*
+csp
+
+csp is a r
+eal realre
+alrealreal
+real compe
+tition.
+
+Come   and
+join   us
+*/
+
+
+
+int w; 
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin>>w;
+	string str;
+	while(getline(cin, str)){
+		
+	}
+	return 0;
+} 
