efb679cef4c70cd9e12c9f06e1bd07a57ecbb7aa
lucas <jxj1950970529@gmail.com>
2021-04-10 21:54:57 +0800

    202004112154

--- 挑战程序设计竞赛/test.cpp
+++ 挑战程序设计竞赛/test.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main(){
-    cout<<"ok"<<endl;
+    int b = 0x3f3f3f3f;
+    int a = -b;
+	cout<<a<<" "<<b<<endl;
+	for(int i=a; i<=b; i++){
+		if(!i) cout<<i<<" "<<"OK!"<<endl;
+	} 
     return 0;
 }
