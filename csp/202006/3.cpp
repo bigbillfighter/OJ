021e35f6c45f03984c3ba75cbfa4dc8ca7101ca9
lucas <jxj1950970529@gmail.com>
2021-04-11 11:25:39 +0800

    202004121125

--- csp/202006/3.cpp
+++ csp/202006/3.cpp
@@ -1,6 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long LL;
+int w; 
+
+bool isblank(string s){
+	for(int i=0; i<s.length(); i++){
+		if(s[i]!=' ') return false;
+	}
+	return true;
+}
+
+string standard(string s){
+	int st, ed;
+	for(st=0; st<s.length(); st++){
+		if(s[st]!=' ') break;	
+	}
+	for(ed = s.length()-1; ed>=0; ed--){
+		if(s[ed]!=' ') break;
+	}
+	return s.substr(st, ed-st+1);
+}
+
+struct Markdown{
+	int type; string s;
+};
+
+vector<Markdown> article;
+int ans;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin>>w;
+	string str;
+	 
+	while(getline(cin, str)){ 
+		if(!isblank(str)) break;	
+	}
+	
+	article.push_back({1, standard(str)}); 
+	bool newpara = false;
+	
+	while(getline(cin, str)){
+		if(isblank(str)){
+			newpara = true;
+			continue;
+		}
+		if(newpara){
+			article.push_back({1, standard(str)}); 
+			newpara = false;
+		}else{
+			Markdown& m = article.back();
+			m.s += " ";
+			m.s += standard(str);
+		}
+	}
+
+	for(int i=0; i<article.size(); i++){
+		string& s = article[i].s;
+		ans++;
+		for(int j=0; j<s.length(); j+=w){
+			while(s[j]==' ') j++;
+			ans++;
+		}
+		
+	}
+	
+	cout<<ans-1<<endl;
+	return 0;
+} 
+
+
 /*
 csp
 
@@ -26,16 +96,16 @@ Come   and
 join   us
 */
 
+/*
+* csp
+    
+*   csp is
+  * a real  
+    competition.
+* 
+  * come!   and   join.
+*tel:
+* 12345
+* 
 
-
-int w; 
-
-int main(){
-	ios::sync_with_stdio(false);
-	cin>>w;
-	string str;
-	while(getline(cin, str)){
-		
-	}
-	return 0;
-} 
+*/
